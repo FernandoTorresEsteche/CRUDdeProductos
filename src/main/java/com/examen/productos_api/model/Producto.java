eb480dff0cd0e2844bcfbe43428907d2641affb2
FernandoTorresEsteche <fate041105@gmail.com>
2025-07-23 17:07:24 -0300

    EXAMEN JAVA WEB

--- src/main/java/com/examen/productos_api/model/Producto.java
+++ src/main/java/com/examen/productos_api/model/Producto.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class Producto {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
